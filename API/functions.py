68d6dbfe8e79b48c7e71d15d7516b2927f2c35b9
sadovea <sadovea@gmail.com>
2024-01-14 13:24:24 +0200

    make changes to API functions

--- API/functions.py
+++ API/functions.py
@@ -1,4 +1,5 @@
 from gift_finder.functions import recommend_product_categories, get_list_product_data
+from gift_finder.models import SelectedGift
 
 
 def process_gift_picking(questionnaire_answers):
@@ -8,3 +9,21 @@ def process_gift_picking(questionnaire_answers):
     return list_of_product_data
 
 
+def convert_to_cents(dollar_string):
+    dollars = float(dollar_string)
+    cents = int(dollars * 100)
+    return cents
+
+
+def fulfill_order(consumption_data):
+    consumpted_gift = SelectedGift.objects.create(
+        name=consumption_data['name'],
+        price=consumption_data['price'],
+        image_url=consumption_data['image_url'],
+        link=consumption_data['link'],
+        is_bought=True,
+    )
+
+    consumpted_gift.save()
+
+    return None
