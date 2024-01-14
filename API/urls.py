831e11319e9f19e4b32546bddcda84feccecab10
sadovea <sadovea@gmail.com>
2024-01-14 18:33:45 +0200

    4

--- API/urls.py
+++ API/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('get_gifts/', views.api_load_response_data),
     path('gifts-archive/', views.load_choosed_gift),
     path('gift_consumption/', views.api_create_checkout_session),
     path('webhooks/stripe/', views.stripe_webhook_view, name='stripe-webhook'),
